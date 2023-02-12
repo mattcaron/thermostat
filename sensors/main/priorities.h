cc7b9e80b2e5a88b5bd37611fb34f2bcbb0125e9
Matthew Caron <matt@mattcaron.net>
2023-02-11 20:35:23 -0500

    Flip priorities of WiFi vs. temperature.
    WiFi having an event should preempt temperature sampling stuff - that
    can be less precise.

--- sensors/main/priorities.h
+++ sensors/main/priorities.h
@@ -9,7 +9,7 @@
 #define __PRIORITIES_H_
 
 #define CONSOLE_TASK_PRIORITY  ((UBaseType_t)(configMAX_PRIORITIES - 2))
-#define WIFI_TASK_PRIORITY     ((UBaseType_t)(configMAX_PRIORITIES - 3))
-#define TEMP_TASK_PRIORITY     ((UBaseType_t)(configMAX_PRIORITIES - 4))
+#define TEMP_TASK_PRIORITY     ((UBaseType_t)(configMAX_PRIORITIES - 3))
+#define WIFI_TASK_PRIORITY     ((UBaseType_t)(configMAX_PRIORITIES - 4))
 
 #endif // __PRIORITIES_H_
